2ae4a338a20b441bac9ed7e1c92e95e3698b8d6e
bluewayne <ljhjay1@163.com>
2017-02-21 15:22:57 +0800

    delete lib

--- service/src/client/web/lib/antd/lib/_util/throttleByAnimationFrame.d.ts
+++
@@ -1,5 +0,0 @@
-export default function throttleByAnimationFrame(fn: any, threshhold?: number): () => void;
-export declare function throttleByAnimationFrameDecorator(threshhold?: number): (target: any, key: any, descriptor: any) => {
-    configurable: boolean;
-    get(): any;
-};
